319f0d0f96c1e62505e6555ce522570d3a52a056
Nightclaw6 <sclaudiu82@yahoo.com>
2022-02-12 22:30:09 -0500

    test1

---
+++ script.js
@@ -0,0 +1,21 @@
+const movieApp = {};
+
+movieApp.apiUrl = 'https://api.themoviedb.org/3';
+movieApp.apiKey = 'fdc64670b61843a5841c94be98a6df3b';
+
+movieApp.init = () => {
+    
+    
+}
+movieApp.getMovies = () => {
+    const url = new URL(movieApp.apiUrl);
+    
+};
+
+
+
+
+
+
+
+movieApp.init();
